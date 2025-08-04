af7adbc498b3e448bd578f856f1656c08ba46c54
Vishwajeet Gour <137927385+GourVishwajeet@users.noreply.github.com>
2025-08-04 13:14:00 +0530

    changse

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc';
 // Replace 'your-repo-name' with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/Portfolio/',
 });
